0178d1b7475316ae3324c898d7b4b94d256d77bd
Johan Hellberg <johan@jhellberg.com>
2021-11-21 18:31:08 +0100

    copied exact source text to scripts.js

--- js/script.js
+++ js/script.js
@@ -1,19 +1,12 @@
-
-
 let items_array = [
-    { "name": "applered", "id": 1, count: 1 },
-    { "name": "applegreen", "id": 2, count: 1 },
-    { "name": "banana", "id": 3, count: 1 },
-    { "name": "orange", "id": 4, count: 1 },
-    { "name": "plumes", "id": 5, count: 1 },
-    { "name": "peach", "id": 6, count: 1 },
-    { "name": "watermelon", "id": 7, count: 1 },
-    { "name": "strawberry", "id": 8, count: 1 },
-    { "name": "blueberry", "id": 9, count: 1 },
+    { "name": "carrots", "id": 1, count: 1 },
+    { "name": "spinach", "id": 2, count: 1 },
+    { "name": "cookies", "id": 3, count: 1 },
+    { "name": "lettuce", "id": 4, count: 1 },
+    { "name": "avocado", "id": 5, count: 1 }
 ];
 
 let cart = [];
-
 
 function appendNode(parent, element) {
     parent.appendChild(element);
@@ -32,15 +25,14 @@ function displayItems(items, container) {
     let items_container = getDiv(container);
     items_container.innerHTML = '';
 
-    for (let i = 0; i < items.lenght; i++) {
+    for (let i = 0; i < items.length; i++) {
         let item = items[i];
 
         let item_node = createNode("li");
-        // may have to modify "li" item to match my own structure
         item_node.setAttribute("id", item.id);
 
         if (item.count > 0) {
-            item_node.innerHTML = `${item.name}
+            item_node.innerHTML = `${item.name} 
             <span id="badge">${item.count}</span>`;
             appendNode(items_container, item_node);
         }
@@ -48,3 +40,4 @@ function displayItems(items, container) {
 }
 
 displayItems(items_array, "items");
+
