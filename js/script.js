e2eef254495d5c5b60acf5d666a56c730cf9effd
Johan Hellberg <johan@jhellberg.com>
2021-11-21 17:58:38 +0100

    add new script file for new shopping cart

---
+++ js/script.js
@@ -0,0 +1,15 @@
+
+
+let items_array = [
+    { "name": "applered", "id": 1, count: 1 },
+    { "name": "applegreen", "id": 2, count: 1 },
+    { "name": "banana", "id": 3, count: 1 },
+    { "name": "orange", "id": 4, count: 1 },
+    { "name": "plumes", "id": 5, count: 1 },
+    { "name": "peach", "id": 6, count: 1 },
+    { "name": "watermelon", "id": 7, count: 1 },
+    { "name": "strawberry", "id": 8, count: 1 },
+    { "name": "blueberry", "id": 9, count: 1 },
+];
+
+let cart = [];
