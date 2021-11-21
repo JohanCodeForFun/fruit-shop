56850fd5a0b7fe4a6fa3b0396471dbe6de6742ad
Johan Hellberg <johan@jhellberg.com>
2021-11-21 18:21:44 +0100

    added new cart.html to follow along tutorial

--- js/script.js
+++ js/script.js
@@ -28,3 +28,23 @@ function createNode(node) {
     return element;
 };
 
+function displayItems(items, container) {
+    let items_container = getDiv(container);
+    items_container.innerHTML = '';
+
+    for (let i = 0; i < items.lenght; i++) {
+        let item = items[i];
+
+        let item_node = createNode("li");
+        // may have to modify "li" item to match my own structure
+        item_node.setAttribute("id", item.id);
+
+        if (item.count > 0) {
+            item_node.innerHTML = `${item.name}
+            <span id="badge">${item.count}</span>`;
+            appendNode(items_container, item_node);
+        }
+    }
+}
+
+displayItems(items_array, "items");
