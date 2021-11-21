54fad594d5d3acdb799332b1cbd90c5c1b1d9136
Johan Hellberg <johan@jhellberg.com>
2021-11-21 18:06:09 +0100

    add new functions for appendNode, getDiv, createNode

--- js/script.js
+++ js/script.js
@@ -13,3 +13,18 @@ let items_array = [
 ];
 
 let cart = [];
+
+
+function appendNode(parent, element) {
+    parent.appendChild(element);
+};
+
+function getDiv(container) {
+    return document.getElementById(container);
+};
+
+function createNode(node) {
+    let element = document.createElement(node);
+    return element;
+};
+
