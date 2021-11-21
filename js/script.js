e7241b17903a0ec9b78e53a2d8874ab566212fa1
Johan Hellberg <johan@jhellberg.com>
2021-11-21 19:01:40 +0100

    continued adding function add or remove form cart

--- js/script.js
+++ js/script.js
@@ -54,4 +54,32 @@ function addOrRemoveItemsFromCart(action) {
 
         takeAction(container)
     };
+
+    // does this code goes here?
+    function takeAction(container) {
+        container.addEventListener("click", function (event) {
+            let item_id = event.target.id;
+
+            if (item_id !== "items" && item_id !== "badge") {
+                let item = items_array.filter(function (item) {
+                    return item.id == item.id;
+                })[0];
+
+                let item_in_cart = cart.filter(function (item) {
+                    return item.id == item.id;
+                })[0];
+
+                if (item_in_cart == undefined) {
+                    cart.push(item);
+                }  else if (action == "add") {
+                    item_in_cart.count++;
+                } else if (action == "remove") {
+                    item_in_cart.count--;
+                }
+
+                console.log(cart);
+                displayItems(cart, "cart");
+            };
+        });
+    };
 }
